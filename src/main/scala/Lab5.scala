84edc3dd04e3a43f81f456e9cba372639931025f
thejinxters <brandon.mikulka@colorado.edu>
2014-11-20 08:49:08 -0700

    Finish MapFirstWith

--- src/main/scala/Lab5.scala
+++ src/main/scala/Lab5.scala
@@ -1,50 +1,53 @@
 object Lab5 extends jsy.util.JsyApplication {
   import jsy.lab5.ast._
   import jsy.lab5._
-  
+
   /*
    * CSCI 3155: Lab 5
-   * <Your Name>
-   * 
-   * Partner: <Your Partner's Name>
+   * Brandon Mikulka
+   *
+   * Partner: Daniel Palmer
    * Collaborators: <Any Collaborators>
    */
 
   /*
    * Fill in the appropriate portions above by replacing things delimited
    * by '<'... '>'.
-   * 
+   *
    * Replace 'YourIdentiKey' in the object name above with your IdentiKey.
-   * 
+   *
    * Replace the 'throw new UnsupportedOperationException' expression with
    * your code in each function.
-   * 
+   *
    * Do not make other modifications to this template, such as
    * - adding "extends App" or "extends Application" to your Lab object,
    * - adding a "main" method, and
    * - leaving any failing asserts.
-   * 
+   *
    * Your lab will not be graded if it does not compile.
-   * 
+   *
    * This template compiles without error. Before you submit comment out any
    * code that does not compile or causes a failing assert.  Simply put in a
    * 'throws new UnsupportedOperationException' as needed to get something
    * that compiles without error.
    */
-  
+
   /*** Helper: mapFirst to DoWith ***/
-  
+
   // Just like mapFirst from Lab 4 but uses a callback f that returns a DoWith in the Some case.
   def mapFirstWith[W,A](f: A => Option[DoWith[W,A]])(l: List[A]): DoWith[W,List[A]] = l match {
-    case Nil => throw new UnsupportedOperationException
+    // If the list is empty, just return the empty list "l"
+    case Nil => doreturn(l)
     case h :: t => f(h) match {
-      case None => throw new UnsupportedOperationException
-      case Some(withhp) => throw new UnsupportedOperationException
+      // If not the chosen element recurse and return the DoWith list
+      case None => mapFirstWith(f)(t) map { x => h :: x}
+      // If "chosen" then just map modified element into the original list and return
+      case Some(withhp) => withhp map (x => x :: t)
     }
   }
 
   /*** Casting ***/
-  
+
   def castOk(t1: Typ, t2: Typ): Boolean = (t1, t2) match {
     case (TNull, TObj(_)) => true
     case (_, _) if (t1 == t2) => true
@@ -53,20 +56,20 @@ object Lab5 extends jsy.util.JsyApplication {
     case (_, TInterface(tvar, t2p)) => castOk(t1, typSubstitute(t2p, t2, tvar))
     case _ => false
   }
-  
+
   /*** Type Inference ***/
-  
+
   def hasFunctionTyp(t: Typ): Boolean = t match {
     case TFunction(_, _) => true
     case TObj(fields) if (fields exists { case (_, t) => hasFunctionTyp(t) }) => true
     case _ => false
-  } 
-    
+  }
+
   def mut(m: PMode): Mutability = m match {
     case PName => MConst
     case PVar | PRef => MVar
   }
-  
+
   def typeInfer(env: Map[String,(Mutability,Typ)], e: Expr): Typ = {
     def typ(e1: Expr) = typeInfer(env, e1)
     def err[T](tgot: Typ, e1: Expr): T = throw new StaticTypeError(tgot, e1, e)
@@ -142,8 +145,8 @@ object Lab5 extends jsy.util.JsyApplication {
       case GetField(e1, f) => typ(e1) match {
         case TObj(tfields) if (tfields.contains(f)) => tfields(f)
         case tgot => err(tgot, e1)
-      } 
-      
+      }
+
       case Function(p, paramse, tann, e1) => {
         // Bind to env1 an environment that extends env with an appropriate binding if
         // the function is potentially recursive.
@@ -164,7 +167,7 @@ object Lab5 extends jsy.util.JsyApplication {
         tann foreach { rt => if (rt != t1) err(t1, e1) };
         TFunction(paramse, t1)
       }
-      
+
       case Call(e1, args) => typ(e1) match {
         case TFunction(Left(params), tret) if (params.length == args.length) => {
           (params, args).zipped.foreach {
@@ -176,16 +179,16 @@ object Lab5 extends jsy.util.JsyApplication {
           throw new UnsupportedOperationException
         case tgot => err(tgot, e1)
       }
-      
+
       /*** Fill-in more cases here. ***/
-        
+
       /* Should not match: non-source expressions or should have been removed */
       case A(_) | Unary(Deref, _) | InterfaceDecl(_, _, _) => throw new IllegalArgumentException("Gremlins: Encountered unexpected expression %s.".format(e))
     }
   }
-  
+
   /*** Small-Step Interpreter ***/
-  
+
   /* Do the operation for an inequality. */
   def inequalityVal(bop: Bop, v1: Expr, v2: Expr): Boolean = {
     require(bop == Lt || bop == Le || bop == Gt || bop == Ge)
@@ -206,7 +209,7 @@ object Lab5 extends jsy.util.JsyApplication {
         }
     }
   }
-  
+
   /* Capture-avoiding substitution in e replacing variables x with esub. */
   def substitute(e: Expr, esub: Expr, x: String): Expr = {
     def subst(e: Expr): Expr = substitute(e, esub, x)
@@ -232,11 +235,11 @@ object Lab5 extends jsy.util.JsyApplication {
   /* A small-step transition. */
   def step(e: Expr): DoWith[Mem, Expr] = {
     require(!isValue(e), "stepping on a value: %s".format(e))
-    
+
     /*** Helpers for Call ***/
-    
+
     def stepIfNotValue(e: Expr): Option[DoWith[Mem,Expr]] = if (isValue(e)) None else Some(step(e))
-    
+
     /* Check whether or not the argument expression is ready to be applied. */
     def argApplyable(mode: PMode, arg: Expr): Boolean = mode match {
       case PVar => isValue(arg)
@@ -266,7 +269,7 @@ object Lab5 extends jsy.util.JsyApplication {
         throw new UnsupportedOperationException
       case GetField(a @ A(_), f) =>
         throw new UnsupportedOperationException
-        
+
       case Call(v1, args) if isValue(v1) =>
         def substfun(e1: Expr, p: Option[String]): Expr = p match {
           case None => e1
@@ -275,8 +278,8 @@ object Lab5 extends jsy.util.JsyApplication {
         (v1, args) match {
           /*** Fill-in the DoCall cases, the SearchCall2, the SearchCallVar, the SearchCallRef  ***/
           case _ => throw StuckError(e)
-        } 
-      
+        }
+
       case Decl(MConst, x, v1, e2) if isValue(v1) =>
         throw new UnsupportedOperationException
       case Decl(MVar, x, v1, e2) if isValue(v1) =>
@@ -284,12 +287,12 @@ object Lab5 extends jsy.util.JsyApplication {
 
       case Assign(Unary(Deref, a @ A(_)), v) if isValue(v) =>
         for (_ <- domodify { (m: Mem) => (throw new UnsupportedOperationException): Mem }) yield v
-        
+
       /*** Fill-in more Do cases here. ***/
-      
+
       /* Base Cases: Error Rules */
       /*** Fill-in cases here. ***/
-        
+
       /* Inductive Cases: Search Rules */
       case Print(e1) =>
         for (e1p <- step(e1)) yield Print(e1p)
@@ -307,7 +310,7 @@ object Lab5 extends jsy.util.JsyApplication {
         case None => throw StuckError(e)
       }
       case GetField(e1, f) => throw new UnsupportedOperationException
-      
+
       /*** Fill-in more Search cases here. ***/
 
       /* Everything else is a stuck error. */
@@ -319,25 +322,25 @@ object Lab5 extends jsy.util.JsyApplication {
 
   this.debug = true // comment this out or set to false if you don't want print debugging information
   this.maxSteps = Some(500) // comment this out or set to None to not bound the number of steps.
-  
+
   def inferType(e: Expr): Typ = {
     if (debug) {
       println("------------------------------------------------------------")
       println("Type checking: %s ...".format(e))
-    } 
+    }
     val t = typeInfer(Map.empty, e)
     if (debug) {
       println("Type: " + pretty(t))
     }
     t
   }
-  
-  // Interface to run your small-step interpreter and print out the steps of evaluation if debugging. 
-  
+
+  // Interface to run your small-step interpreter and print out the steps of evaluation if debugging.
+
   case class TerminationError(e: Expr) extends Exception {
     override def toString = Parser.formatErrorMessage(e.pos, "TerminationError", "run out of steps in evaluating " + e)
   }
-  
+
   def iterateStep(e: Expr): Expr = {
     require(closed(e), "not a closed expression: free variables: %s".format(freeVars(e)) )
     def loop(e: Expr, n: Int): DoWith[Mem,Expr] =
@@ -365,7 +368,7 @@ object Lab5 extends jsy.util.JsyApplication {
 
   // Convenience to pass in a jsy expression as a string.
   def iterateStep(s: String): Expr = iterateStep(removeInterfaceDecl(jsy.lab5.Parser.parse(s)))
-  
+
   // Interface for main
   def processFile(file: java.io.File) {
     if (debug) {
@@ -373,29 +376,29 @@ object Lab5 extends jsy.util.JsyApplication {
       println("File: " + file.getName)
       println("Parsing ...")
     }
-    
+
     val expr =
       handle(None: Option[Expr]) {Some{
         jsy.lab5.Parser.parseFile(file)
       }} getOrElse {
         return
       }
-      
+
     val exprlowered =
       handle(None: Option[Expr]) {Some{
         removeInterfaceDecl(expr)
       }} getOrElse {
         return
-      }  
-    
+      }
+
     handle() {
       val t = inferType(exprlowered)
     }
-    
+
     handle() {
       val v1 = iterateStep(exprlowered)
       println(pretty(v1))
     }
   }
-    
+
 }
