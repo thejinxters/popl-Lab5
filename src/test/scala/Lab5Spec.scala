e64805aefd013403354971efc4b4cb6ed02f6a7a
thejinxters <brandon.mikulka@colorado.edu>
2014-12-05 10:49:02 -0700

    Stregthen Tests Suite

--- src/test/scala/Lab5Spec.scala
+++ src/test/scala/Lab5Spec.scala
@@ -71,15 +71,26 @@ class Lab5Spec extends FlatSpec {
   }
 
   "DoArith" should "add two numbers" in {
-    val x = Binary(Seq, N(1), Binary(Plus, N(5), N(8)))
-    val(mp:Mem, ep:Expr) = step(e)(Mem.empty) 
+    val e = Binary(Seq, N(1), Binary(Plus, N(5), N(8)))
+    val(mp:Mem, ep:Expr) = step(e)(Mem.empty)
     assert(mp.isEmpty)
     assertResult(Binary(Plus, N(5), N(8))) {ep}
   }
 
-  "DoPlusString" should "add two strings" in {
-
+  "DoArith" should "sum numbers" in {
+    val e = Binary(Plus, N(2), N(3))
+    val (mp:Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(mp.isEmpty)
+    assertResult(N(5)) { ep }
   }
+
+  "DoPlusString" should "concat strings" in {
+    val e = Binary(Plus, S("abc"), S("xyz"))
+    val (mp:Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(mp.isEmpty)
+    assertResult(S("abcxyz")) { ep }
+  }
+
 
   "DoInequalityNumber" should "compare two numbers" in {
 
@@ -93,9 +104,13 @@ class Lab5Spec extends FlatSpec {
 
   }
 
-  "DoAndTrue" should "return value of second value" in {
-
+  "DoAndTrue" should "not shortcircuiting boolean and" in {
+    val e = Binary(And, B(true), Decl(MVar, "x", B(true), Var("x")))
+    val (mp:Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(mp.isEmpty)
+    assertResult(Decl(MVar, "x", B(true), Var("x"))) { ep }
   }
+
 
   "DoAndFalse" should "return false" in {
 
@@ -164,6 +179,15 @@ class Lab5Spec extends FlatSpec {
     assertResult(N(47)) {epp}
   }
 
+  "DoAssignField" should "access a field from an object in memory" in {
+    val setup = Obj(Map("a" -> N(42), "b" -> N(47)))
+    val (m:Mem, a: A) = step(setup)(Mem.empty)
+    val e = Assign(GetField(a, "b"), N(99))
+    val (mp, ep: Expr) = step(e)(m)
+    assertResult(Obj(Map("a" -> N(42), "b" -> N(99)))) { mp get a get}
+    assertResult(N(99)) { ep }
+  }
+
 
   "DoCall" should "call a function" in {
 
@@ -209,6 +233,30 @@ class Lab5Spec extends FlatSpec {
     }
   }
 
+  "SearchDecl" should "step its declared value" in {
+    val e = Decl(MConst,"x",Binary(Times,N(6),N(7)),Undefined)
+    val (mp: Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(mp.isEmpty)
+    assertResult(Decl(MConst,"x",N(42),Undefined)) { ep }
+  }
+
+  "SearchAssign1" should "step the lhs" in {
+    //  { a: 42, b: 47 }.a = 99
+    val e = Assign(GetField(Obj(Map("a" -> N(42.0), "b" -> N(47.0))),"a"),N(99.0))
+    val (mp: Mem, ep: Expr) = step(e)(Mem.empty)
+
+  }
+
+  "SearchAssign2" should "step the rhs" in {
+    val e = Decl(MVar,"x",N(42),Assign(Var("x"),N(47)))
+    val (mp: Mem, ep: Expr) = step(e)(Mem.empty)
+    assert(ep match {
+      case Assign(Unary(Deref, a @ A(_)),N(47)) =>
+        // Verify memory correctly references N(42)
+        mp.get(a).get == N(42)
+      case _ => false
+    })
+  }
 
 
 
